3f83a110e1b145e57fdecd6ca7aeb06645efcdc6
aqibabdullahdev@gmail.com <aqibabdullahdev@gmail.com>
2025-07-20 23:54:37 +0500

    Alerts and Routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,88 @@
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import TextArea from "./components/TextArea";
+import Alert from "./components/Alert";
+import {
+  Route,
+  index,
+  layout,
+  prefix,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [btnText, setBtnText] = useState("Dark Mode");
+  const [clsLabel, setClsLabel] = useState("dark");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
+  const handleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+      setBtnText("Dark Mode");
+      setClsLabel("dark");
+      showAlert("Light mode has been enabled", "success");
+      document.body.style.backgroundColor = "#FFFFFF";
+    } else {
+      setTheme("dark");
+      setBtnText("Light Mode");
+      setClsLabel("light");
+      showAlert("Dark mode has been enabled", "success");
+      document.body.style.backgroundColor = "#2B3035";
+    }
+  };
   return (
     <>
-      <Navbar title="TextUtils" />
-      <div className="container">
-        <TextArea header="Enter text to analyze" />
-      </div>
+      <BrowserRouter>
+        <Navbar
+          title="TextUtils"
+          theme={theme}
+          toogleTheme={handleTheme}
+          btnText={btnText}
+          clsLabel={clsLabel}
+        />
+        <Alert alert={alert} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <TextArea
+                header="Enter text to analyze"
+                theme={theme}
+                clsLabel={clsLabel}
+                showAlert={showAlert}
+                style={{
+                  color: theme === "dark" ? "#FFFFFF" : "#2B3035",
+                }}
+              />
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <About
+                theme={theme}
+                style={{
+                  color: theme === "dark" ? "#FFFFFF" : "#2B3035",
+                }}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function TextArea(props) {
   const [text, setText] = useState("Enter Your text here");
   const [mail, setMail] = useState([""]);
-  const [fword, setFword] = useState("");
+  // const [fword, setFword] = useState("");
 
   const handleUpperCase = () => {
     let newText = text.toUpperCase();
@@ -22,14 +22,14 @@ export default function TextArea(props) {
   const handleEmailFinder = () => {
     let word = text.split(" ");
     let emails = [];
-      word.forEach((element) => {
-        if (element.includes("@")) {
-          emails.push(element);
-          setMail(emails.join(', '));
-        } else {
-          setMail([""]);
-        }
-      });
+    word.forEach((element) => {
+      if (element.includes("@")) {
+        emails.push(element);
+        setMail(emails.join(", "));
+      } else {
+        setMail([""]);
+      }
+    });
   };
 
   const handleRverseText = () => {
@@ -40,50 +40,55 @@ export default function TextArea(props) {
 
   const handleCopyToClipboard = () => {
     navigator.clipboard.writeText(text);
+    props.showAlert("Copied To Clipboard","success");
   };
 
-  const handleFindText = () => {};
+  // const handleFindText = () => {};
 
-  const handleReplaceText = () => {};
+  // const handleReplaceText = () => {};
 
   const handleClearAll = () => {
     setText("");
   };
   return (
     <>
-      <div className="container">
-        <h1>{props.header}</h1>
-        <div className="mb-3">
-          <textarea
-            className="form-control"
-            value={text}
-            onChange={handleOnChange}
-            id="myBox"
-            rows="3"
-          ></textarea>
-        </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpperCase}>
-          To Uppercase
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleLowerCase}>
-          To Lowercase
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleEmailFinder}>
-          Find Email
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleRverseText}>
-          Reverse Text
-        </button>
-        <button
-          className="btn btn-primary mx-2"
-          onClick={handleCopyToClipboard}
-        >
-          Copy to Clipboard
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleClearAll}>
-          Clear All
-        </button>
-        <div className="mb-3">
+      <div
+        className="container"
+        style={props.style}
+      >
+        <div className="container">
+          <h1>{props.header}</h1>
+          <div className="mb-3" data-bs-theme={props.theme}>
+            <textarea
+              className="form-control"
+              value={text}
+              onChange={handleOnChange}
+              id="myBox"
+              rows="3"
+            ></textarea>
+          </div>
+          <button className="btn btn-primary mx-2" onClick={handleUpperCase}>
+            To Uppercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleLowerCase}>
+            To Lowercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleEmailFinder}>
+            Find Email
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleRverseText}>
+            Reverse Text
+          </button>
+          <button
+            className="btn btn-primary mx-2"
+            onClick={handleCopyToClipboard}
+          >
+            Copy to Clipboard
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleClearAll}>
+            Clear All
+          </button>
+          {/* <div className="mb-3">
           <textarea
             className="form-control"
             onChange={handleFindText}
@@ -96,24 +101,25 @@ export default function TextArea(props) {
         </button>
         <div className="mb-3">
           <textarea
-            className="form-control"
-            onChange={handleFindText}
+          className="form-control"
+          onChange={handleFindText}
             id="myBox"
             rows="1"
-          ></textarea>
-        </div>
+            ></textarea>
+            </div>
         <button className="btn btn-primary" onClick={handleReplaceText}>
-          Replace
-        </button>
-      </div>
-      <div class="container my-3">
-        <h1>Text Summary</h1>
-        <p>Number of words {text.split(" ").length}</p>
-        <p>Number of characters {text.length}</p>
-      </div>
-      <div class="container my-2">
-        <h1>Email from the text</h1>
-        <p>Emails: {mail}</p>
+        Replace
+        </button> */}
+        </div>
+        <div class="container my-3">
+          <h1>Text Summary</h1>
+          <p>Number of words {text.split(" ").length}</p>
+          <p>Number of characters {text.length}</p>
+        </div>
+        <div class="container">
+          <h1>Email from the text</h1>
+          <p>Emails: {mail}</p>
+        </div>
       </div>
     </>
   );
